bde0b08316f43b3cbe02887dc2af5fd2f81ded3e
Rafael Ribeiro <rafaelcar203@gmail.com>
2024-06-05 00:27:14 -0300

    login display implemented

---
+++ meu-projeto/src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    background-color: #1C1C1C;
+    font-family: Arial, Helvetica, sans-serif
+  }
+`;
+
+export default GlobalStyle;
