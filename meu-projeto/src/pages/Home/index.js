1d26e8a486342a218b3319c3335692653fe2988a
Rafael Ribeiro <rafaelcar203@gmail.com>
2024-06-10 23:28:45 -0300

    user home implemented

--- meu-projeto/src/pages/Home/index.js
+++ meu-projeto/src/pages/Home/index.js
@@ -1,20 +1,74 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "../../components/Button";
 import useAuth from "../../hooks/useAuth";
-import * as C from "./styles";
+import * as C from "./styles"; // Certifique-se de que os componentes estão exportados corretamente aqui
+import leojardim from "../../img/leojardim.png"; // Corrija os nomes das imagens
+import dvd from "../../img/dvd.png";
+import maicon from "../../img/maicon.png"
+import logo from "../../img/logo.png";
+import { GlobalStyle } from './styles'; // Importe o GlobalStyle aqui
 
 const Home = () => {
   const { signout } = useAuth();
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    signout(); // Isso irá limpar quaisquer tokens de autenticação
+    navigate('/signin'); // Isso irá redirecionar o usuário para a tela de login
+  };
+
   return (
-    <C.Container>
-      <C.Title>Home</C.Title>
-      <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
-        Sair
-      </Button>
-    </C.Container>
+    <>
+      <GlobalStyle />
+      <C.LogoutButton onClick={handleLogout}>SAIR</C.LogoutButton>
+      <C.logo src = {logo} alt="logo" title="logo"/>
+      <C.TopBar />
+      
+      {/*barra de filtro*/}
+      <C.TelaInteira>
+        <C.SearchCard>
+          Filtrar por:
+        </C.SearchCard>
+        <C.Container>
+          {/* Exemplo de perfil para Leo Jardins */}
+          <C.ProfileCard>
+              <C.ProfileImage src={leojardim} alt="Leo Jardins" />
+              <C.ProfileName>
+                Leo Jardins
+                <C.ProfileDetails>32 anos - Vitória, Espírito Santo</C.ProfileDetails>
+              </C.ProfileName>
+              <C.ProfileDescription>
+                Descrição breve sobre Leo Jardins ou sua experiência profissional Descrição breve sobre Leo Jardins ou sua experiência profissional Descrição breve sobre Leo Jardins ou sua experiência profissional
+              </C.ProfileDescription>
+          </C.ProfileCard>
+
+          {/* Exemplo de perfil para David Correa */}
+          <C.ProfileCard>
+              <C.ProfileImage src={dvd} alt="David Correa" />
+              <C.ProfileName>
+                David Correa
+                <C.ProfileDetails>37 anos - Majur, Espírito Santo</C.ProfileDetails>
+              </C.ProfileName>
+              <C.ProfileDescription>Descrição breve sobre David Correa ou sua experiência profissional.</C.ProfileDescription>
+              
+          </C.ProfileCard>
+          {/* Exemplo de perfil para David Correa */}
+          <C.ProfileCard>
+            <C.ProfileImage src={maicon} alt="Maicon" />
+            <C.ProfileName>
+              Maicon 
+              <C.ProfileDetails>37 anos - Jaguare, Espírito Santo</C.ProfileDetails>
+            </C.ProfileName>
+            <C.ProfileDescription>maicon eles nao ligam pra gente</C.ProfileDescription>
+          </C.ProfileCard>
+          {/* Exemplo de perfil para David Correa */}
+          {/* Adicione mais perfis conforme necessário */}
+
+          {/* Botão para sair */}
+          
+        </C.Container>
+      </C.TelaInteira>
+    </>
   );
 };
 
--- meu-projeto/src/pages/Home/styles.js
+++ meu-projeto/src/pages/Home/styles.js
@@ -1,12 +1,134 @@
 import styled from "styled-components";
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+`
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
+  justify-content: flex-start;
+  padding-left: 0px;
   height: 100vh;
-  gap: 20px;
+  width: 100vw; // Adicionado para ocupar toda a largura da tela
+  overflow: hidden; // Adicionado para evitar rolagem
+  background-color: #F5F5F5; // Altere para a cor de fundo desejada
 `;
 
-export const Title = styled.h2``;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333; // Altere para a cor desejada
+`;
+
+export const SearchCard = styled.div`
+  margin-left: 50px;
+  margin-top:20px;
+  display: flex;
+  flex-direction: line;
+  align-items: left;
+  width: 100%;
+  max-width: 350px;
+  padding-left: 50px;
+  background-color: #FFF; // Altere para a cor de fundo desejada
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); // Altere para a sombra desejada
+  border-radius: 10px; // Altere para o raio de borda desejado
+  margin-bottom: 450px;
+`;
+
+export const TelaInteira = styled.div`
+  display: flex;
+  flex-direction: line;
+  justify-content: flex-start;
+  padding-left: 0px;
+  height: 100vh;
+  width: 100vw; // Adicionado para ocupar toda a largura da tela
+  overflow: hidden; // Adicionado para evitar rolagem
+  background-color: #F5F5F5; // Altere para a cor de fundo desejada
+`;
+
+export const ProfileCard = styled.div`
+  margin-left: 130px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: line;
+  align-items: left;
+  width: 100%;
+  max-width: 1300px;
+  padding: 40px;
+  max-padding: 40px;
+  background-color: #FFF; // Altere para a cor de fundo desejada
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); // Altere para a sombra desejada
+  border-radius: 10px; // Altere para o raio de borda desejado
+  margin-bottom: 20px;
+  gap: 10px;
+`;
+
+export const ProfileImage = styled.img`
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+export const ProfileName = styled.h2`
+  margin-left: 15px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #333; // Altere para a cor desejada
+  margin-top: 10px;
+  max-width: 150px;
+`;
+
+export const ProfileDetails = styled.p`
+  margin-top: 8px;
+  font-size: 15px;
+  color: #1C1C1C; // Altere para a cor desejada
+`;
+
+export const ProfileDescription = styled.p`
+  position: center;
+  margin-left: 300px;
+  font-size: 14px;
+  color: #1D1D1D; // Altere para a cor desejada
+  max-width: 500px;
+  margin-top: 10px;
+`;
+
+export const TopBar = styled.div`
+  width: 100%;
+  height: 80px; // Ajuste a altura conforme necessário
+  background-color: #1C1C1C; // Ajuste a cor conforme necessário
+  positon: fixed
+  top: 0;
+  left: 0;
+  z-index: 1000; // Isso garante que a barra esteja sempre no topo
+`;
+export const logo = styled.img`
+  position: absolute;
+  width: 179px; /* Defina a largura desejada */
+  height: 110px; /* Mantém a proporção da imagem */
+  display: block; /* Garante que a imagem seja um bloco */
+  margin-left: 15px;
+  margin-top: -14px;
+`;
+export const LogoutButton = styled.button`
+  position: absolute;
+  right: 100px; // Ajuste conforme necessário
+  top: 27px; // Ajuste conforme necessário
+  background: none;
+  color: #FFFFFF; /* Cor do texto */
+  border: none;
+  cursor: pointer;
+  font-size: 18px; /* Ajuste o tamanho da fonte conforme necessário */
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
