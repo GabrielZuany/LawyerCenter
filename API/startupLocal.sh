e708b36b9633a68e7a935113e62343862974fc58
GabrielZuany <gzuanydev@gmail.com>
2024-06-23 01:25:03 -0300

    add scripts to run all locally or using port forwarding ('remote')

---
+++ API/startupLocal.sh
@@ -0,0 +1,4 @@
+# Script to initialize the API locally (LINUX)
+
+sudo docker compose -f ../Databases/PgSQL/docker-compose.yml up -d
+sudo docker rm lc_api_container | docker build . -t lc_api_img && docker run --network host --name lc_api_container -p 5001:5001 -e ASPNETCORE_URLS=http://+:5001 -e ASPNETCORE_ENVIRONMENT=Development lc_api_img
---
+++ API/startupPortForwarding.sh
@@ -0,0 +1,2 @@
+sudo ssh -f -N -L 5432:localhost:5432 -i /home/zuany/Desktop/dev/auth/oracle/sudo/ssh-rsa-30-03-2024.key ubuntu@168.138.151.184
+sudo ssh -f -N -L 5001:localhost:5001 -i /home/zuany/Desktop/dev/auth/oracle/sudo/ssh-rsa-30-03-2024.key ubuntu@168.138.151.184
