4a83935d4ee024851c9638380d16e408a12b346b
GabrielZuany <gzuanydev@gmail.com>
2024-07-08 19:01:43 -0300

    W.I.P: api tests

---
+++ API.Tests/RoutesTests/LawyerControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using API.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace API.Tests.RoutesTests
+{
+    public class LawyerControllerTests
+    {
+        [Fact]
+        public void CreateTest()
+        {
+
+        }
+    }
+}
